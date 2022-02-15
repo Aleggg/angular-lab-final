975082cd7b8d6dbacf283e28490aa49d16d1b927
Oleh <olehhasii@gmail.com>
2022-02-15 12:49:36 +0200

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { GamesComponent } from './components/games/games.component';
+import { LibraryComponent } from './components/library/library.component';
+import { FriendsComponent } from './components/friends/friends.component';
+import { LibraryGamesComponent } from './components/library-games/library-games.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginPageComponent,
+    GamesComponent,
+    LibraryComponent,
+    FriendsComponent,
+    LibraryGamesComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
